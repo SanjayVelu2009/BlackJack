52c2f85ca119994cd8214d892e17e19bf07f0d2b
SanjayVelu2009 <svelu967@student.fuhsd.org>
2025-05-08 15:44:56 -0700

    Update Deck.java

--- Deck.java
+++ Deck.java
@@ -1,20 +1,34 @@
-public class Deck
+public class Deck 
 {
 	
-	private Card[] cardStack = new Card[52];	
+	private Card[] cardStack = new Card[52];
+		
 	private int topCard;
+	
 	
 	public Deck()
 	{
 		topCard = 0;
 		
-		initializeDeck();
-	
-		shuffleDuck();
-		//System.out.println("Shuffled");
 	}
 	
-	private void initializeDeck()
+	public static void main(String[] args)
+	{
+		Deck testDeck = new Deck();
+		testDeck.runIt();
+
+		
+	}
+		
+	public void runIt()
+	{
+		initializeDeck();
+		
+		
+		
+	}
+	
+	public void initializeDeck()
 	{
 		int cardIndex = 0;
 		
@@ -45,33 +59,55 @@ public class Deck
 			Card spadeCard = new Card(j,'S');
 			cardStack[cardIndex++] = spadeCard;
 		}
+		
+		String[] nameStack = {"1H.jpg","2H.jpg","3H.jpg","4H.jpg","5H.jpg","6H.jpg","7H.jpg","8H.jpg","9H.jpg","10H.jpg","11H.jpg","12H.jpg","13H.jpg","1C.jpg","2C.jpg","3C.jpg","4C.jpg","5C.jpg","6C.jpg","7C.jpg","8C.jpg","9C.jpg","10C.jpg","11C.jpg","12C.jpg","13C.jpg"
+							+"1D.jpg","2D.jpg","3D.jpg","4D.jpg","5D.jpg","6D.jpg","7D.jpg","8D.jpg","9D.jpg","10D.jpg","11D.jpg","12D.jpg","13D.jpg","1S.jpg","2S.jpg","3S.jpg","4S.jpg","5S.jpg","6S.jpg","7S.jpg","8S.jpg","9S.jpg","10S.jpg","11S.jpg","12S.jpg","13S.jpg"};
+		shuffleDeckAndImage(nameStack);
 	}
 	
 	public  void showDeck()
 	{
-		for(int i = 0; i<52; i++)
+		for(int i = 0; i<=51; i++)
 		{
 			cardStack[i].show();
 		}
 	}
 	
-	public void shuffleDuck()
+	public void shuffleDeckAndImage(String[] nameStack)
 	{
 		int b;
+		String swapImage = "";
 		
-		for(int a = 0; a<52; a++)
+		
+		
+		for(int a = 0; a<nameStack.length; a++)
 		{
-			b = (int)(Math.random()*(52));
+			b = (int)(Math.random()*(51));
 			
 			if(b != a)
 			{
 				Card swapCard = cardStack[a];
 				cardStack[a] = cardStack[b];
 				cardStack[b] = swapCard;
+				//shuffling Card deck
+				
+				swapImage = nameStack[a];
+				nameStack[a] = nameStack[b];
+				nameStack[b] = swapImage;
+				
+				//shuffling names of images
 			}
+			
+			System.out.println(nameStack[a]);
 		}
+		
+		
 	}
 	
+	public void generateImage()
+	{
+		
+	}
 	public Card dealCard()
 	{
 		Card deal = cardStack[topCard];
